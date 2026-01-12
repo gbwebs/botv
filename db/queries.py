29f8582e60eb422ed9ae0ce4394cbe276af33753
shadowhunter1312 <visuareal.ghanshyam@gmail.com>
2026-01-12 13:00:02 +0530

    tetsp

--- db/queries.py
+++ db/queries.py
@@ -1,4 +1,4 @@
 from db.database import execute
 
 async def clear_users_table():
-    await execute("TRUNCATE TABLE users RESTART IDENTITY")
+    await execute("TRUNCATE TABLE links, users RESTART IDENTITY")
